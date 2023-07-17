c5b631b48e6f9f84515720f7a78aefff5067c683
Paul <puscasspaul@yahoo.com>
2023-07-17 17:04:58 +0300

    Initial Commit

---
+++ Tips-Split-the-bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tips-Split-the-bill
+//
+//  Created by Puscas Paul on 17.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
